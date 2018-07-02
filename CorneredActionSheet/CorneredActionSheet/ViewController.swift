63a2a6d515c84781c2e5b841de78849941b20c59
Sindhu - iOS <sindhu-ios@Movius-BLR-MackBook-Pro.local>
2018-07-02 21:51:04 +0530

    initial project commit

---
+++ CorneredActionSheet/CorneredActionSheet/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CorneredActionSheet
+//
+//  Created by Sindhu - iOS on 02/07/18.
+//  Copyright © 2018 Sindhu - iOS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
